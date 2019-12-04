b19e6fbaa8a9dfb153d47072ede7b4e635a72865
Marvin.Min <marvin.min@activenetwork.com>
2019-12-04 16:54:35 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import BgImage from './1.png'
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  const bg = new Image();
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  bg.src = BgImage;
+  element.appendChild(bg)
+  return element;
+}
+
+document.body.appendChild(component());
