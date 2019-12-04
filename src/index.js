e5176429aeb8d35b7d785f277bebe8aa9b7619b4
Marvin.Min <marvin.min@activenetwork.com>
2019-12-04 20:48:39 +0800

    output manage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js'
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var btn = document.createElement('button')
+  btn.innerHTML = 'Click then check console'
+  btn.onclick = printMe
+  element.appendChild(btn)
   return element;
 }
 
