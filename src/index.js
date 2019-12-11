568b3b4b15591415249a953a378493fb90587e8f
Marvin.Min <marvin.min@activenetwork.com>
2019-12-11 14:04:34 +0800

    dynamic

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ function component() {
   element.classList.add('hello');
 
   var btn = document.createElement('button')
-  btn.innerHTML = 'Click then check console'
+  btn.innerHTML = 'Click then check console------->'
   btn.onclick = printMe
   element.appendChild(btn)
   return element;
 }
 
 document.body.appendChild(component());
+
+if(module.hot){
+  module.hot.accept('./print.js',function(){
+    console.log("->>>>>>>>>>>>");
+    printMe();
+  });
+}
